0ac520a8a900842f036654a39df1846ff92d7f81
Jefferson Tora <hi@jeffersontora.com>
2020-04-20 19:57:31 -0300

    Add Binary Search Challenge

---
+++ src/binarySearch/binarySearch.js
@@ -0,0 +1,11 @@
+/**
+ * Binary Search
+ * Given a list of ordered numbers, search for the given number.
+ * For example, given [1, 2, 3, 34, 54, 56, 78] you need to find the number 3, the algorithm needs to return 2 (position on the array).
+ * If the target is not found, it needs to return -1.
+ *
+ */
+
+function binarySearch(list, target) {}
+
+export default binarySearch;
