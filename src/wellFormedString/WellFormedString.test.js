5e43bdeea145654a3773915ef180154bae46bfd9
Jefferson Tora <hi@jeffersontora.com>
2020-04-20 18:13:01 -0300

    Fix Well formed string tests

--- src/wellFormedString/WellFormedString.test.js
+++ src/wellFormedString/WellFormedString.test.js
@@ -4,14 +4,20 @@ describe("Well formed string", () => {
   test("Should return TRUE when is given '([])[]({})' ", () => {
     expect(wellFormedString("([])[]({})")).toBe(true);
   });
+
+  test("Should return TRUE when is given '([])([{}])({})' ", () => {
+    expect(wellFormedString("([])([{}])({})")).toBe(true);
+  });
+
   test("Should return FALSE when is given '((()' ", () => {
     expect(wellFormedString("((()")).toBe(false);
   });
+
   test("Should return FALSE when is given '([)]' ", () => {
     expect(wellFormedString("([)]")).toBe(false);
   });
 
-  test("Should return TRUE when is given '([])([{}])({})' ", () => {
-    expect(wellFormedString("([])([{}])({})")).toBe(true);
+  test("Should return FALSE when is given '}()[]' ", () => {
+    expect(wellFormedString("}()[]")).toBe(false);
   });
 });
