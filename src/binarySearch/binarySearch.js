3be530df7b4c09b6806f89bd1d4f9e47db0198ad
Jefferson Tora <hi@jeffersontora.com>
2020-04-21 16:12:52 -0300

    Add Binary Search Challenge solution

--- src/binarySearch/binarySearch.js
+++ src/binarySearch/binarySearch.js
@@ -6,6 +6,32 @@
  *
  */
 
-function binarySearch(list, target) {}
+function binarySearch(list, target) {
+  let pointL = 0;
+  let pointR = list.length - 1;
+  let middlePoint = Math.floor((pointL + pointR) / 2);
+
+  while (true) {
+    //Depends on the programming language you are using you need to check if the array size (list) is bigger than middlePoint list.length > middlePoint to stop while
+    let middleElement = list[middlePoint];
+
+    if (middleElement === target) break;
+
+    if (target < middleElement) {
+      pointR = middlePoint - 1;
+    }
+    if (target > middleElement) {
+      pointL = middlePoint + 1;
+    }
+
+    if (pointL > pointR) {
+      middlePoint = -1;
+      break;
+    }
+    middlePoint = Math.floor((pointL + pointR) / 2);
+  }
+
+  return middlePoint;
+}
 
 export default binarySearch;
