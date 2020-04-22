f71f9b474a080a1293b5b77e4a3f54dd1d2ca6f2
Jefferson Tora <hi@jeffersontora.com>
2020-04-22 19:50:08 -0300

    Add the Sum Array Deep Challenge

---
+++ src/sumArrayDeep/sumArrayDeep.js
@@ -0,0 +1,10 @@
+/**
+ * Sum Array Deep
+ * Given a list of numbers and/or other lists, return the sum multiplied for the current deep.
+ * For example, given [1, 2, 3, [5, 6], 7] the result will be 35.
+ *
+ */
+
+const sumArrayDeep = (list, deeph) => {};
+
+export default sumArrayDeep;
